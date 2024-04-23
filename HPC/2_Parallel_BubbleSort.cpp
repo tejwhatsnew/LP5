8c768b394498baf13a17cb3f261b6d7408c1c0fd
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2024-04-23 20:20:17 +0530

    Add files via upload

---
+++ HPC/2_Parallel_BubbleSort.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<omp.h>
+
+using namespace std;
+
+void bubble(int array[], int n){
+    for (int i = 0; i < n - 1; i++){
+        for (int j = 0; j < n - i - 1; j++){
+            if (array[j] > array[j + 1]) swap(array[j], array[j + 1]);
+        }
+    }
+}
+
+void pBubble(int array[], int n){
+    //Sort odd indexed numbers
+    for(int i = 0; i < n; ++i){    
+        #pragma omp for
+        for (int j = 1; j < n; j += 2){
+        if (array[j] < array[j-1])
+        {
+          swap(array[j], array[j - 1]);
+        }
+    }
+
+    // Synchronize
+    #pragma omp barrier
+
+    //Sort even indexed numbers
+    #pragma omp for
+    for (int j = 2; j < n; j += 2){
+      if (array[j] < array[j-1])
+      {
+        swap(array[j], array[j - 1]);
+      }
+    }
+  }
+}
+
+void printArray(int arr[], int n){
+    for(int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << "\n";
+}
+
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    
+    int arr[n];
+    double start_time, end_time;
+
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    // Sequential time
+    start_time = omp_get_wtime();
+    bubble(arr, n);
+    end_time = omp_get_wtime();     
+    cout << "Sequential Bubble Sort took : " << end_time - start_time << " seconds.\n";
+    printArray(arr, n);
+
+    cout<<"\n Enter the elements of the array again: \n";
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    // Parallel time
+    start_time = omp_get_wtime();
+    pBubble(arr, n);
+    end_time = omp_get_wtime();     
+    cout << "Parallel Bubble Sort took : " << end_time - start_time << " seconds.\n";
+    printArray(arr, n);
+}   
